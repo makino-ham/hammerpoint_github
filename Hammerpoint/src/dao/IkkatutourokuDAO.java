1e7d8647a35659aff77bc1342a5a930b99cf770b
makino-ham <92966611+makino-ham@users.noreply.github.com>
2021-11-05 14:06:59 +0900

    1105牧野(テスト)

--- Hammerpoint/src/dao/IkkatutourokuDAO.java
+++ Hammerpoint/src/dao/IkkatutourokuDAO.java
@@ -1,20 +1,26 @@
-//package dao;
-//
-//import java.io.IOException;
-//import java.nio.charset.StandardCharsets;
-//import java.nio.file.Files;
-//import java.util.List;
-//
-//public class IkkatutourokuDAO {
-////	List型じゃないらしいので変更する
-//	public static void Ikkatutouroku(String[] args) {
-////	    Path path = Paths.get(PATH);
-////	    File file = path.toFile();
-//
-//		try {
-////		    List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-////		} catch (IOException e) {
-////		    e.printStackTrace();
-//		}
-//	}
-//		}
+package dao;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class IkkatutourokuDAO {
+private static final URI PATH = null;
+
+	//	List型じゃないらしいので変更する
+	public static void Ikkatutouroku(String[] args) {
+	    Path path = Paths.get(PATH);
+	    File file = path.toFile();
+
+		try {
+		    List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+		}
