cf84d589c9cd7fc6c74c43134903d6584e1d6987
makino-ham <92966611+makino-ham@users.noreply.github.com>
2021-11-09 14:16:32 +0900

    Update gakkaDAO.java

--- Hammerpoint/src/dao/gakkaDAO.java
+++ Hammerpoint/src/dao/gakkaDAO.java
@@ -40,7 +40,7 @@ public class gakkaDAO {
 			//結果表に格納されたレコードの内容をgakkaインスタンスに設定し、ArrayListインスタンスに追加
 			while(rs.next()) {
 				int id = rs.getInt("gakka_ID");
-				String name = rs.getString("gakka_name");
+				String name = rs.getString("gakka_Name");
 
 				//gakka方の変数に、取得したデータを引数として代入→リストに追加
 				Gakka gakka = new Gakka(id, name);
@@ -67,5 +67,36 @@ public class gakkaDAO {
 		return gakkaList;
 	}
 
+	public boolean create(Gakka gakka) {
+		Connection conn = null;
 
+		try {
+			//データベースへ接続
+			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(DRIVER_NAME);
+			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+
+			//INSERT文の準備(gakka_idは自動連番なので指定しなくてよい
+			String sql = "INSERT INTO GAKKA(GAKKA_NAME) VALUES(?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//INSERT文中の「?」に使用する値を設定しSQLを完成
+			pStmt.setString(1, gakka.getGakkaName());
+
+			//INSERT文を実行(result二は追加された行数が代入される)
+			int result = pStmt.executeUpdate();
+			//1ではない(追加データがない)場合
+			if (result != 1) {
+				return false;
+			}
+		//エラーチェック
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 }
